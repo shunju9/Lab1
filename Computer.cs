28532f269c3645e407fdc4583255edfa338e4aec
buturlin <156885757+Glanvi@users.noreply.github.com>
2025-01-16 12:35:55 +0300

    Добавьте файлы проекта.

---
+++ Computer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Computer
+    {
+        public int InventoryNumber;
+        public double ProcessorFrequency; 
+        public int HardDriveSize; 
+
+        public void ShowComputerInfo()
+        {
+            Console.WriteLine("Информация о компьютере:");
+            Console.WriteLine($"Инвентарный номер: {InventoryNumber}");
+            Console.WriteLine($"Частота процессора: {ProcessorFrequency} ГГц");
+            Console.WriteLine($"Объем жесткого диска: {HardDriveSize} Гб");
+        }
+
+        public double CalculateStorageCost(double pricePerGB)
+        {
+            return HardDriveSize * pricePerGB;
+        }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Computer myComputer = new Computer();
+
+            
+            myComputer.InventoryNumber = 123345;
+            myComputer.ProcessorFrequency = 1.5;
+            myComputer.HardDriveSize = 400;
+
+            
+            myComputer.ShowComputerInfo();
+
+           
+            double pricePerGB = 100;
+            double storageCost = myComputer.CalculateStorageCost(pricePerGB);
+
+           
+            Console.WriteLine($"Стоимость внешней памяти: {storageCost} рублей");
+            Console.ReadKey(); 
+        }
+    }
+}
